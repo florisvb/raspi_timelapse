9a985c7ebd5bd9023da7628f2f4356508c05f463
Floris van Breugel <florisvb@gmail.com>
2016-03-15 23:37:03 +0000

    using different mount approach now

--- timelapse.py
+++ timelapse.py
@@ -8,27 +8,16 @@ def write_image():
 
 	camera = picamera.PiCamera()
 	camera.resolution = (2592,1944)
-	
-	cmd = 'ls /media/pi'
-	ls = os.popen(cmd).read()
-	drives = ls.split('\n')
-	
-	if len(drives) > 0:
-		for drive in drives:
-			if len(drive) < 1:
-				continue
-			path = os.path.join('/media/pi', drive)
-			image_path = os.path.join(path, 'images')
-			if not os.path.exists(image_path):
-				os.mkdir(image_path)
-		
-			image_name = time.strftime('%Y%m%d_%H%M%S.jpg', time.gmtime())
-			image_name_with_path = os.path.join(image_path, image_name)
-			camera.capture(image_name_with_path)
-			camera.close()
 
-	else:
-		print 'No media device!'
+	path = '/media/usb'
+	image_path = os.path.join(path, 'images')
+	if not os.path.exists(image_path):
+		os.mkdir(image_path)
+
+	image_name = time.strftime('%Y%m%d_%H%M%S.jpg', time.gmtime())
+	image_name_with_path = os.path.join(image_path, image_name)
+	camera.capture(image_name_with_path)
+	camera.close()
 	
 if __name__ == '__main__':
 	dt = 10
